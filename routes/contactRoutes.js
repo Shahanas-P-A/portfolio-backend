b040dd5e342755ac410f3232aacd7a9496a36242
shahanas <sanapa1417@gmail.com>
2026-06-28 21:24:36 +0530

    Fix model import case

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Contact from "../models/Contact.js";
+import Contact from "../models/contact.js";
 import transporter from "../config/mailer.js";
 
 const router = express.Router();
